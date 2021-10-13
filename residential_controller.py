b91a754c3bb1012008a7c90f21906fd8da95dabf
LukaTrudel <luka.trudel@gmail.com>
2021-10-12 22:39:37 -0400

    scenarios are starting to workgit add .git add .git add .git add .git add .

--- residential_controller.py
+++ residential_controller.py
@@ -294,7 +294,7 @@ def scenario2():
     print("______________________________________________________________________________________________")
     print()
     print("--------------------SCENARIO #2--------------------")
-    column = Column(1, 'online', 10, 2)
+    column = Column(1, ColumnStatus.ACTIVE, 10, 2)
     column.display()
     column.elevatorsList[0].currentFloor = 10
     column.elevatorsList[1].currentFloor = 3
@@ -325,27 +325,3 @@ def scenario2():
 scenario2()
 
 
-# def scenario2(): 
-#     print()
-#     print("****************************** SCENARIO 2: ******************************")
-#     columnScenario2 = Column(1, ColumnStatus.ACTIVE, 10, 2)
-#     columnScenario2.display()  
-#     columnScenario2.elevatorsList[0].floor = 10
-#     columnScenario2.elevatorsList[1].floor = 3
-    
-#     print()
-#     print("Person 1: (elevator 2 is expected)")
-#     columnScenario2.requestElevator(1, ButtonDirection.UP)
-#     columnScenario2.elevatorsList[1].requestFloor(6, columnScenario2)
-#     print("----------------------------------")
-#     print()
-#     print("Person 2: (elevator 2 is expected)")
-#     columnScenario2.requestElevator(3, ButtonDirection.UP)
-#     columnScenario2.elevatorsList[1].requestFloor(5, columnScenario2)
-#     print("----------------------------------")
-#     print()
-#     print("Person 3: (elevator 1 is expected)")
-#     columnScenario2.requestElevator(9, ButtonDirection.DOWN)
-#     columnScenario2.elevatorsList[0].requestFloor(2, columnScenario2)
-#     print("==================================")
-# scenario2()
